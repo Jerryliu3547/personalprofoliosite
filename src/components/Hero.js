27cde76c7810e8a91ea06243129de67c03976134
Jerryliu3547 <liudianze1994@gmail.com>
2024-08-15 23:22:48 -0700

    add acerdity ui

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { cn } from "@/lib/utils";
+import { Spotlight } from "./ui/Sportlight";
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+          <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen ' fill='white' />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
