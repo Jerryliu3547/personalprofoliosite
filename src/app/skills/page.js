f5ad1d571d32bb7f44f578a4d86cd4f0a955608c
Jerryliu3547 <liudianze1994@gmail.com>
2024-08-24 22:05:43 -0700

    add logo source and add all logo to skill page card

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -5,72 +5,78 @@ import { BentoGrid, BentoGridItem } from "../../components/ui/bento-grid.js";
 import { motion } from "framer-motion";
 import Image from 'next/image'
 import {Stack, Avatar} from '@mui/material'
+import {sourcelist1, sourcelist2, sourcelist3, sourcelist4, sourcelist5, sourcelist6, sourcelist7, sourcelist8, sourcelist9
+} from './components/sourcelist'
 
 
 
 const page = () => {
-  const Skeleton = () => (
+  const Skeleton = ({ sources=[] }) => (
     <div className="flex items-center justify-center w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
-      <Stack direction="row" justifyContent="center" alignItems="center">
-        <Avatar src="/logo/JavaScript.png" sx={{ width: 80, height: 80, borderRadius: '50%' }} />
+      <Stack direction="row" justifyContent="center" alignItems="center" gap={2}>
+        {sources.map((source, index) => (
+          <Avatar variant='rounded' key={index} src={source} sx={{ width: 80, height: 80, borderRadius: '50%' }} />
+        ))}
       </Stack>
     </div>
   );
+
+
   
   
   const items = [
     {
       title: "JavaScript",
       description: "5 Years Expreience",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist1}/>,
   
     },
     {
       title: "React & Next.JS",
       description: "5 Years Experience",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist2}/>,
    
     },
     {
       title: "Styling",
       description: "CSS, Tailwind CSS, Material UI, Aceternity UI",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist3} />,
 
     },
     {
       title: "BackEnd",
       description:
         "Express",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist4} />,
 
     },
     {
       title: "Database",
       description:
         "MongoDB & SQL",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist5} />,
 
     },
     {
       title: "Python",
       description: "8 Years Exprerience",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist6} />,
     },
     {
       title: "AI Framework and Library",
       description: "TensorFlow, Scikit Learn, Keras",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist7} />,
  
     },
     {
       title: "C++",
       description: "3 Years of Exprience",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist8} />,
     },
     {
       title: "JAVA",
       description: "1 Years of Exprience",
-      header: <Skeleton />,
+      header: <Skeleton sources={sourcelist9} />,
     },
   ];
   return (
