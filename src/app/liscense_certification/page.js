51494fbc36b502bd472e7d351cab6e925500d428
Jerryliu3547 <liudianze1994@gmail.com>
2024-08-29 00:12:34 -0700

    give a temp page to liscense page

--- src/app/liscense_certification/page.js
+++ src/app/liscense_certification/page.js
@@ -10,28 +10,35 @@ import 'swiper/css/pagination';
 
 const licensePage = () => {
   return (
-    <div> 
-      <h1>License Page</h1>
-      <Link href="https://we-go-again.com/?ref=landing.love">Link to sample</Link>
-        <Swiper
-        direction={'vertical'}
-        mousewheel= {true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+    // <div> 
+    //   <h1>License Page</h1>
+    //   <Link href="https://we-go-again.com/?ref=landing.love">Link to sample</Link>
+    //     <Swiper
+    //     direction={'vertical'}
+    //     mousewheel= {true}
+    //     pagination={{
+    //       clickable: true,
+    //     }}
+    //     modules={[Pagination]}
+    //     className="mySwiper"
+    //   >
+    //     <SwiperSlide>Slide 1</SwiperSlide>
+    //     <SwiperSlide>Slide 2</SwiperSlide>
+    //     <SwiperSlide>Slide 3</SwiperSlide>
+    //     <SwiperSlide>Slide 4</SwiperSlide>
+    //     <SwiperSlide>Slide 5</SwiperSlide>
+    //     <SwiperSlide>Slide 6</SwiperSlide>
+    //     <SwiperSlide>Slide 7</SwiperSlide>
+    //     <SwiperSlide>Slide 8</SwiperSlide>
+    //     <SwiperSlide>Slide 9</SwiperSlide>
+    //   </Swiper>
+    // </div>
+    <div className="flex justify-center items-center h-screen">
+      <Link href="https://we-go-again.com/?ref=landing.love">
+        <a className="text-white px-10 py-5 rounded-md shadow-md hover:shadow-lg">
+          Click here to explore more
+        </a>
+      </Link>
     </div>
   )
 }
